384b64fe5db1bb3c28ad4f6a53d75d76665e2440
Parsa Hosseini <officer47p@gmail.com>
2026-09-17 21:29:58 +0330

    Using custom resources for upgrades

--- scenes/manager/experience_manager.gd
+++ scenes/manager/experience_manager.gd
@@ -3,6 +3,8 @@ extends Node
 const TARGET_EXPERIENCE_GROWTH = 5
 
 signal experience_updated(current_experience: float, target_experience: float)
+signal level_up(new_level: int)
+
 
 var current_experience = 0
 var current_level = 1
@@ -14,12 +16,12 @@ func _ready() -> void:
 func increment_experience(number: float):
 	current_experience = min(current_experience + number, target_experience)
 	experience_updated.emit(current_experience, target_experience)
-	print(current_experience, target_experience)
 	if current_experience == target_experience:
 		current_level += 1
 		target_experience += TARGET_EXPERIENCE_GROWTH
 		current_experience = 0
 		experience_updated.emit(current_experience, target_experience)
+		level_up.emit(current_level)
 
 func on_experience_vial_collected(number: float):
 	increment_experience(number)
