ca7b221fc779950ca1f2f768764db24325605b72
Parsa Hosseini <officer47p@gmail.com>
2026-09-21 15:43:15 +0330

    Enabling upgrade selection

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -1,9 +1,19 @@
 extends PanelContainer
 
+signal selected
+
 @onready var name_label: Label = $VBoxContainer/NameLabel
 @onready var description_label: Label = $VBoxContainer/DescriptionLabel
 
 
+func _ready() -> void:
+	gui_input.connect(on_gui_input)
+
 func set_ability_upgrade(ability: AbilityUpgrade):
 	name_label.text = ability.name
 	description_label.text = ability.description
+
+func on_gui_input(event: InputEvent):
+	if event.is_action_pressed("left_click"):
+		selected.emit()
+		
